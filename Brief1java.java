cc4bdb43a7c3d35bf1f308252698b6bba08132d4
oumaimach20 <oumaimachaggour@gmail.com>
2024-02-09 15:52:38 +0100

    classe apprenant

---
+++ Brief1java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.brief1java;
+
+/**
+ *
+ * @author user
+ */
+public class Brief1java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
